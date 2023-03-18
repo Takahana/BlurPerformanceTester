ae2ef1d901b97f2588a224f363b0f9dcd08494ad
Go Takahana <tagahana508@gmail.com>
2023-03-19 01:14:23 +0900

    add RemoteImage

---
+++ app/src/main/java/tech/takahana/blurperformancetester/domain/domainobject/RemoteImage.kt
@@ -0,0 +1,7 @@
+package tech.takahana.blurperformancetester.domain.domainobject
+
+object RemoteImage {
+  val W8256_H5504 = "https://imgsv.nikon-image.com/products/slr/lineup/d850/img/sample/pic_01_og.jpg"
+  val W1920_H1080 = "https://placehold.jp/1920x1080.png"
+  val W160_H90 = "https://placehold.jp/160x90.png"
+}
