3e7057b575e88ec1053f7da078e24a178e9130da
Go Takahana <tagahana508@gmail.com>
2023-03-19 09:27:02 +0900

    display screen size

--- app/src/main/java/tech/takahana/blurperformancetester/components/render/ComposeRenderFragment.kt
+++ app/src/main/java/tech/takahana/blurperformancetester/components/render/ComposeRenderFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +15,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
@@ -26,6 +29,7 @@ import coil.request.ImageRequest
 import tech.takahana.blurperformancetester.R
 import tech.takahana.blurperformancetester.components.fragment.setContentOnFragment
 import tech.takahana.blurperformancetester.databinding.FragmentComposeRenderBinding
+import tech.takahana.blurperformancetester.domain.domainobject.ImageSize
 import tech.takahana.blurperformancetester.domain.domainobject.RemoteImage
 
 class ComposeRenderFragment : Fragment(R.layout.fragment_compose_render) {
@@ -67,9 +71,20 @@ fun ComposeRenderScreen() {
   var imageHeight by remember { mutableStateOf(0) }
   var renderState by remember { mutableStateOf(RenderState.Initialized) }
 
+  val configuration = LocalConfiguration.current
+  val density = LocalDensity.current
+  val screenWidth = density.run {
+    configuration.screenWidthDp.dp.roundToPx()
+  }
+  val screenHeight = density.run {
+    configuration.screenHeightDp.dp.roundToPx()
+  }
+
   Column(
     modifier = Modifier.fillMaxSize()
   ) {
+    Text(text = "screenWidth: $screenWidth")
+    Text(text = "screenHeight: $screenHeight")
     Text(text = "width: $imageWidth")
     Text(text = "height: $imageHeight")
     when (renderState) {
